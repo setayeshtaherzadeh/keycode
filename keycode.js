fc3b89d83d54680fdb5adfc4da0d12e4894f9a9c
setayeshtaherzadeh <107579513+setayeshtaherzadeh@users.noreply.github.com>
2023-05-18 13:38:33 +0330

    Add files via upload

---
+++ keycode.js
@@ -0,0 +1,29 @@
+﻿let $ = document
+let locationKey = $.querySelector('#location')
+let title = $.querySelector('title')
+
+let keyCodeElem = $.getElementById('keyCode')
+let keyElem = $.getElementById('key')
+let whichElem = $.getElementById('which')
+let codeElem = $.getElementById('code')
+let locationElem = $.getElementById('location')
+
+document.body.addEventListener('keydown', function (event) {
+	event.preventDefault()
+	
+	starter.style.display = 'none'
+	heading.style.display = 'flex'
+	ascii.style.display = 'flex'
+	infos.style.display = 'flex'
+	let eventkey = event.key
+	let eventwhich = event.which
+    let eventlocation = event.location
+	let eventcode = event.code
+	let eventkeycode = event.keyCode
+	keyCodeElem.innerHTML = eventkeycode
+	keyElem.innerHTML = eventkey
+	whichElem.innerHTML = eventwhich
+	codeElem.innerHTML = eventcode
+	locationElem.innerHTML = eventlocation
+
+})
